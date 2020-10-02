7ca1515ecf71fb44e50ac611701a4f68d58475da
alak <alak@estatelogs.com>
2020-10-02 08:20:15 +0300

    updated :rocket:

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React, { useEffect, useState } from 'react';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import './App.css';
+
+function App() {
+  const [state, setstate] = useState([])
+  useEffect(() => {
+    getData()
+  }, [])
+  const getData = async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/order-list/');
+    const data = await response.json()
+    console.log(data);
+    setstate(data)
+  }
+  console.log("state", state);
+
+  return (
+    <div className="App">
+      {
+        state.map((i: any) => {
+          return <p> {i.customer.name}</p>
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
